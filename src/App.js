882bac32f95cd7aed812e828c9f2487fffd714bd
Atul Gautam <userAtul@mxLinux>
2020-09-18 07:10:14 +0530

    fixLogo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./App.css";
 import Banner from "./Components/Banner";
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,31 +2,29 @@ import React, { useEffect, useState } from "react";
 import "../style/Nav.css";
 
 const Nav = () => {
-    const [show,handleShow] = useState()
+  const [show, handleShow] = useState();
   useEffect(() => {
-      window.addEventListener('scroll',()=>{
-          if(window.scrollY>100){
-              handleShow(true)
-          }
-          else handleShow(false)
-      })
-      return ()=>{
-          window.removeEventListener('scroll')
-      }
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
   }, []);
 
   return (
-
-    <div className={`nav ${show&&"nav_black"}`}>
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
-        src="https://upload.wikimedia.org/wikipedia/common/0/of"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
       />
       <img
         className="nav_avtar"
-        src="https://pbs.twing.com/profile_image/124011999041155"
-        alt="netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Netflix_meaningful_logo.svg/1024px-Netflix_meaningful_logo.svg.png"
+        alt="netflix logo Avtar"
       />
     </div>
   );
